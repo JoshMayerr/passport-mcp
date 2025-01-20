853622d27b7df6a4704930e6197849a53242c0d6
Josh Mayer <jooshmayer@gmail.com>
2025-01-19 22:20:03 -0800

    remove bloat

--- sdks/python/scripts/setup-dev.sh
+++ sdks/python/scripts/setup-dev.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 set -e
 
-# Create and activate a virtual environment if it doesn't exist
-if [ ! -d ".venv" ]; then
-    echo "Creating virtual environment..."
-    python -m venv .venv
-fi
-
-# Activate virtual environment
-source .venv/bin/activate
-
-# Upgrade pip and install the package in editable mode with dev dependencies
-python -m pip install --upgrade pip
+# Install the package in development mode
 pip install -e ".[dev]"
-
-echo "Development environment setup complete!"
-echo "To activate the virtual environment, run: source .venv/bin/activate"
